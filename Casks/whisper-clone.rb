b0fc1dc4eb8496beaa035d95dca5ab202bcc82ec
AaryanPurohit <aaryan.purohit1@gmail.com>
2026-03-31 19:10:25 -0400

    Add whisper-clone cask v1.0.0

---
+++ Casks/whisper-clone.rb
@@ -0,0 +1,25 @@
+cask "whisper-clone" do
+  version "1.0.0"
+
+  sha256 "ee1f67e787c29c49e4a37eb30cc0c23217b52bac12cf6e4cb3ade5102a451d22"
+
+  url "https://github.com/AaryanPurohit/Whisper-clone-Package/releases/download/v#{version}/WhisperClone.zip"
+  name "Whisper Clone"
+  desc "System-wide push-to-talk voice dictation for macOS using OpenAI Whisper"
+  homepage "https://github.com/AaryanPurohit/Whisper-clone-Package"
+
+  depends_on macos: ">= :ventura"
+
+  app "WhisperClone.app"
+
+  postflight do
+    system_command "/usr/bin/open", args: ["-a", "WhisperClone"]
+  end
+
+  uninstall quit: "com.aaryanpurohit.whisper-clone"
+
+  zap trash: [
+    "~/Library/Preferences/com.aaryanpurohit.whisper-clone.plist",
+    "~/Library/Application Support/WhisperClone",
+  ]
+end
